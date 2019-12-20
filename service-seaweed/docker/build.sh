b6e69bd20526928b3df0903b44f56c543fa6adda
施骁 <shxdev@163.com>
2019-12-20 23:46:44 +0800

    init

--- service-seaweed/docker/build.sh
+++ service-seaweed/docker/build.sh
@@ -20,9 +20,9 @@ shift $(($OPTIND - 1))
 [[ -z ${TARGET} ]] && TARGET='prod'
 
 DESC="WEEDFS镜像"
-IMAGE_NAME="msy/seaweedfs:prod"
+IMAGE_NAME="shx/seaweedfs:prod"
 DOCKERFILE_NAME="Dockerfile"
-BUILD_PATH=$SH_FOLDER/seaweedfs
+BUILD_PATH=$SH_FOLDER
 echo "[${DESC}]"
 echo "  ${DESC}构建路径: "${BUILD_PATH}
 echo "  开始构建${DESC}"
@@ -31,34 +31,34 @@ exit_if_error $?
 echo -e "  ${DESC}构建完毕 ( image-name: ${IMAGE_NAME} )\n"
 
 
-DESC="文件服务镜像"
-IMAGE_NAME="msy/app-fstore:prod"
-DOCKERFILE_NAME="prod.Dockerfile"
-BUILD_PATH=$SH_FOLDER/build
-APP_SRC_PATH=${SH_FOLDER}/../src
-echo "[${DESC}]"
-echo "  ${DESC}构建路径: "${BUILD_PATH}
-echo "  开始构建${DESC}"
+# DESC="文件服务镜像"
+# IMAGE_NAME="shx/app-fstore:prod"
+# DOCKERFILE_NAME="prod.Dockerfile"
+# BUILD_PATH=$SH_FOLDER/build
+# APP_SRC_PATH=${SH_FOLDER}/../src
+# echo "[${DESC}]"
+# echo "  ${DESC}构建路径: "${BUILD_PATH}
+# echo "  开始构建${DESC}"
 
-rsync -q -av --delete $APP_SRC_PATH/*-requirements.txt ${SH_FOLDER}/build/temp \
-&& rsync -q -av --copy-dirlinks --delete --exclude="pages" --exclude="node_modules" --exclude=".git" --exclude="deploy" --exclude="*.pyc" $APP_SRC_PATH/* ${SH_FOLDER}/build/temp/app
-exit_if_error $?
+# rsync -q -av --delete $APP_SRC_PATH/*-requirements.txt ${SH_FOLDER}/build/temp \
+# && rsync -q -av --copy-dirlinks --delete --exclude="pages" --exclude="node_modules" --exclude=".git" --exclude="deploy" --exclude="*.pyc" $APP_SRC_PATH/* ${SH_FOLDER}/build/temp/app
+# exit_if_error $?
 
-if [ -d $APP_SRC_PATH/pages/vue ];then
-    if [ ! -d $APP_SRC_PATH/pages/vue/dist ] || [ -z ${NOBUILD} ];then
-        cd $APP_SRC_PATH/pages/vue \
-        && cnpm i \
-        && npm run build \
-        && cd -
-        exit_if_error $?
-    fi
-    rsync -q -av $APP_SRC_PATH/pages/vue/dist/. ${SH_FOLDER}/build/temp/pages 
-    exit_if_error $?
-fi
+# if [ -d $APP_SRC_PATH/pages/vue ];then
+#     if [ ! -d $APP_SRC_PATH/pages/vue/dist ] || [ -z ${NOBUILD} ];then
+#         cd $APP_SRC_PATH/pages/vue \
+#         && cnpm i \
+#         && npm run build \
+#         && cd -
+#         exit_if_error $?
+#     fi
+#     rsync -q -av $APP_SRC_PATH/pages/vue/dist/. ${SH_FOLDER}/build/temp/pages 
+#     exit_if_error $?
+# fi
 
-docker build -t ${IMAGE_NAME} -f ${BUILD_PATH}/${DOCKERFILE_NAME} ${BUILD_PATH}
-exit_if_error $?
-echo -e "  ${DESC}构建完毕 ( image-name: ${IMAGE_NAME} )\n"
-rm -rf ${SH_FOLDER}/build/temp
+# docker build -t ${IMAGE_NAME} -f ${BUILD_PATH}/${DOCKERFILE_NAME} ${BUILD_PATH}
+# exit_if_error $?
+# echo -e "  ${DESC}构建完毕 ( image-name: ${IMAGE_NAME} )\n"
+# rm -rf ${SH_FOLDER}/build/temp
 
 
